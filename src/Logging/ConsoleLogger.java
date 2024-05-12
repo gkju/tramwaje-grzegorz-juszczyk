a67a9b852f4e31b34ef3c63708b4e09c0372e783
gkju <gkjuszczyk@gmail.com>
2024-05-12 23:54:19 +0200

    Fix indexing

--- src/Logging/ConsoleLogger.java
+++ src/Logging/ConsoleLogger.java
@@ -20,7 +20,7 @@ public class ConsoleLogger extends Logger {
         var tram = arrivalEvent.getTram();
         var line = tram.getLine();
         System.out.printf("%d, %02d:%02d: Tramwaj o numerze bocznym %d, linii numer %d, przyjechał na przystanek" +
-                " %s, w tramwaju jest %d osób, a na przystanku %d miejsc\n", arrivalEvent.getDate().getDate(), arrivalEvent.getDate().getHours(), arrivalEvent.getDate().getMinutes(), tram.getSerialNumber(), line.getNumber(), arrivalEvent.getStop().getName(), tram.getPassengers().length, arrivalEvent.getStop().getPlacesLeft());
+                " %s, w tramwaju jest %d osób, a na przystanku %d miejsc\n", arrivalEvent.getDate().getDate() - 1, arrivalEvent.getDate().getHours(), arrivalEvent.getDate().getMinutes(), tram.getSerialNumber(), line.getNumber(), arrivalEvent.getStop().getName(), tram.getPassengers().length, arrivalEvent.getStop().getPlacesLeft());
     }
 
     @Override
@@ -44,28 +44,28 @@ public class ConsoleLogger extends Logger {
 
     @Override
     public void logPassengerArrivedAtStop(Passenger passenger, Stop stop, PassengerArrivalEvent event) {
-        System.out.printf("%d, %02d:%02d: Pasażer %s przyszedł na przystanek %s \n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), stop.getName());
+        System.out.printf("%d, %02d:%02d: Pasażer %s przyszedł na przystanek %s \n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), stop.getName());
     }
 
     @Override
     public void logPassengerTramBoarding(Passenger passenger, Tram tram, StopArrivalEvent event) {
-        System.out.printf("%d, %02d:%02d: Pasażer %s wsiadł do tramwaju o numerze bocznym %d, linii numer %d, na przystanku %s z zamiarem dojechania na przystanek %s\n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), tram.getSerialNumber(), tram.getLine().getNumber(), event.getStop().getName(), passenger.getDestinationStop().getName());
+        System.out.printf("%d, %02d:%02d: Pasażer %s wsiadł do tramwaju o numerze bocznym %d, linii numer %d, na przystanku %s z zamiarem dojechania na przystanek %s\n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), tram.getSerialNumber(), tram.getLine().getNumber(), event.getStop().getName(), passenger.getDestinationStop().getName());
 
     }
 
     @Override
     public void logPassengerLeftTram(Passenger passenger, StopArrivalEvent event, Stop stop) {
-        System.out.printf("%d, %02d:%02d: Pasażer %s wysiadł na przystanku %s \n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), event.getStop().getName());
+        System.out.printf("%d, %02d:%02d: Pasażer %s wysiadł na przystanku %s \n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), event.getStop().getName());
     }
 
     @Override
     public void logForcedPassengersOffTram(Tram tram, Event event) {
-        System.out.printf("%d, %02d:%02d: Pod koniec dnia zmuszono %d pasażerów do opuszczenia tramwaju o numerze bocznym %d \n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), tram.getPassengersCount(), tram.getSerialNumber());
+        System.out.printf("%d, %02d:%02d: Pod koniec dnia zmuszono %d pasażerów do opuszczenia tramwaju o numerze bocznym %d \n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), tram.getPassengersCount(), tram.getSerialNumber());
     }
 
     @Override
     public void logPassengersWentHome(Stop stop, Event event) {
-        System.out.printf("%d, %02d:%02d: Pod koniec dnia z przystanku %s wróciło %d pasażerów \n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), stop.getName(), stop.getPassengersCount());
+        System.out.printf("%d, %02d:%02d: Pod koniec dnia z przystanku %s wróciło %d pasażerów \n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), stop.getName(), stop.getPassengersCount());
     }
 
     @Override
@@ -82,10 +82,10 @@ public class ConsoleLogger extends Logger {
 
     @Override
     public void logDayStats(Simulator simulator, Event event) {
-        var prompt = String.format("--------------Statystyki dla dnia %d--------------\n", event.getDate().getDate());
+        var prompt = String.format("--------------Statystyki dla dnia %d--------------\n", event.getDate().getDate() - 1);
         dayStats.append(prompt);
         System.out.printf(prompt);
-        prompt = String.format("Dnia %d odbyło się %d przejazdów i czekano %d minut na przystankach.\n", event.getDate().getDate(), simulator.getDayTrips(), simulator.getDayWaitTime());
+        prompt = String.format("Dnia %d odbyło się %d przejazdów i czekano %d minut na przystankach.\n", event.getDate().getDate() - 1, simulator.getDayTrips(), simulator.getDayWaitTime());
         System.out.printf(prompt);
         dayStats.append(prompt);
         prompt = String.format("Średni czas oczekiwania na przystanku wynosił %f minut\n", simulator.getLastDayAverageWaitTime().getAverage());
@@ -104,6 +104,6 @@ public class ConsoleLogger extends Logger {
 
     @Override
     public void logPassengerFailedToStayAtStop(Passenger passenger, Stop stop, PassengerArrivalEvent event) {
-        System.out.printf("%d, %02d:%02d: Pasażer %s nie dał rady przyjść na przystanek %s\n", event.getDate().getDate(), event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), stop.getName());
+        System.out.printf("%d, %02d:%02d: Pasażer %s nie dał rady przyjść na przystanek %s\n", event.getDate().getDate() - 1, event.getDate().getHours(), event.getDate().getMinutes(), passenger.getName(), stop.getName());
     }
 }
